346ebf87ca38789d8223b5575f02a98edc938002
Timo <timo.nesensohn@student.htldornbirn.at>
2021-11-19 11:14:13 +0100

    remote tested

--- src/at/Viereckig/projects/oo/Fernbedienung/Main.java
+++ src/at/Viereckig/projects/oo/Fernbedienung/Main.java
@@ -2,8 +2,8 @@ package at.Viereckig.projects.oo.Fernbedienung;
 
 public class Main {
 	public static void main(String[] args) {
-		Battery b1 = new Battery();
-		Battery b2 = new Battery();
+		Battery b1 = new Battery(60);
+		Battery b2 = new Battery(85);
 		
 		remote r = new remote();
 		r.setB1(b1);
