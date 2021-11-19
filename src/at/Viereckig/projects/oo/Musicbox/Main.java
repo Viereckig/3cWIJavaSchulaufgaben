89a5c2ab56dfc1c7e7a26fc91b08b9d23504afeb
Timo <timo.nesensohn@student.htldornbirn.at>
2021-11-19 12:17:19 +0100

    musicbox

---
+++ src/at/Viereckig/projects/oo/Musicbox/Main.java
@@ -0,0 +1,17 @@
+package at.Viereckig.projects.oo.Musicbox;
+
+public class Main {
+	
+}
+
+
+/*
+Kann 50 platten haben
+addRecord(record) 			damit kann platte hinzugefügt werden(wenn schon 50 vorhanden fehler)
+removeRecord(record)		damit kann platte entfernt werden
+getSumOfMusic()				wird gesamte Spielzeit aller Platten ausgegeben
+searchRecord(title)			kann Plate gesucht werden
+loadRecord(record)			wird platte in spieler gelegt
+play(titelnummer)			wird titel an der stelle abgespielt und gibt es aus
+Wird play gestartet ohne eine Platte im Spieler kommt ein Fehler. 
+*/
